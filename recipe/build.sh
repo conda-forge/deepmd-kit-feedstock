4574a77dcc5475072109b475082891aee915b7d8
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-10-11 13:04:04 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 set -e
 SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION pip install . --no-deps -vv
+
+if [[ "$target_platform" == linux* ]]; then
+# no libtensorflow_cc on osx
+
 mkdir source/build
 cd source/build
 cmake -D TENSORFLOW_ROOT=${PREFIX} \
@@ -7,3 +11,5 @@ cmake -D TENSORFLOW_ROOT=${PREFIX} \
 	  ..
 make -j${CPU_COUNT}
 make install
+
+fi
