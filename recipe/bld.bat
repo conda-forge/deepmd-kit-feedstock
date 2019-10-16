61cdc06d5032eca4ba6ef3936dab469811285864
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-10-16 05:46:54 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
-pip install --install-option="-- -G %CMAKE_GENERATOR%" . --no-deps -vv
+pip install --install-option="-G'NMake Makefiles'" . --no-deps -vv
