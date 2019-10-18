bcdb78078aff28dd89d3b7294e374dcca58871c5
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-10-18 16:07:31 -0400

    Update recipe/bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
-pip install --install-option="\"-GNMake Makefiles\"" . --no-deps -vv
+pip install --install-option="-GNMake Makefiles" . --no-deps -vv
