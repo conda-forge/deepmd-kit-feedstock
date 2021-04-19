571c8054a805cefd3da76f35a2ee760d88041e22
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-19 18:36:25 -0400

    out of memory

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,14 +12,14 @@ cd $SRC_DIR/source/build
 cmake -D TENSORFLOW_ROOT=${PREFIX} \
 	  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
 	  $SRC_DIR/source
-make -j${CPU_COUNT}
+make #-j${CPU_COUNT}
 make install
 make lammps
 cp -r $SRC_DIR/source/build/USER-DEEPMD $SRC_DIR/lammps/src
 mkdir -p $SRC_DIR/lammps/build
 cd $SRC_DIR/lammps/build
 cmake -D PKG_USER-DEEPMD=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -lrt -Wl,-rpath=${PREFIX}/lib" $SRC_DIR/lammps/cmake
-make -j${NUM_CPUS}
+make #-j${NUM_CPUS}
 make install
 
 fi
