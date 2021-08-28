272a3eed8bf01282f3a04c8b27f333e92b41ffe2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-08-28 13:48:13 -0400

    fix build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,7 @@ make lammps
 cp -r $SRC_DIR/source/build/USER-DEEPMD $SRC_DIR/lammps/src
 mkdir -p $SRC_DIR/lammps/build
 cd $SRC_DIR/lammps/build
-cmake -D PKG_USER-DEEPMD=ON -D PKG_KSPACE=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -lrt -Wl,-rpath=${PREFIX}/lib" $SRC_DIR/lammps/cmake
+cmake -D PKG_USER-DEEPMD=ON -D PKG_KSPACE=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DLAMMPS_VERSION_NUMBER=20201029 -DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -ldeepmd_cc -ltensorflow_cc -ltensorflow_framework -lrt -Wl,-rpath=${PREFIX}/lib" $SRC_DIR/lammps/cmake
 make #-j${NUM_CPUS}
 make install
 
