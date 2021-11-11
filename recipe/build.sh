016d7399542a387c11080d96f6ff4bc20d1db809
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-11-11 16:11:46 -0500

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,22 +4,17 @@ SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION pip install . --no-deps -vv
 if [[ "$target_platform" == linux* ]]; then
 # no libtensorflow_cc on osx
 
-# copy absl headers to prevent conflict
-rsync -avzh --include '*/' --include '*.h' --include '*.inc' --exclude '*' $SRC_DIR/absl/absl/ $PREFIX/include/absl/
-
 mkdir $SRC_DIR/source/build
 cd $SRC_DIR/source/build
+if [[ ${cuda_compiler_version} != "None" ]]; then
+    DEEPMD_USE_CUDA_TOOLKIT=TRUE
+else
+    DEEPMD_USE_CUDA_TOOLKIT=FALSE
+fi
 cmake -D TENSORFLOW_ROOT=${PREFIX} \
 	  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+      -D USE_CUDA_TOOLKIT=${DEEPMD_USE_CUDA_TOOLKIT} \
+	  -D LAMMPS_SOURCE_ROOT=$SRC_DIR/lammps \
 	  $SRC_DIR/source
 make #-j${CPU_COUNT}
 make install
-make lammps
-cp -r $SRC_DIR/source/build/USER-DEEPMD $SRC_DIR/lammps/src
-mkdir -p $SRC_DIR/lammps/build
-cd $SRC_DIR/lammps/build
-cmake -D PKG_USER-DEEPMD=ON -D PKG_KSPACE=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DLAMMPS_VERSION_NUMBER=20201029 -DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -ldeepmd_cc -ltensorflow_cc -ltensorflow_framework -lrt -Wl,-rpath=${PREFIX}/lib" $SRC_DIR/lammps/cmake
-make #-j${NUM_CPUS}
-make install
-
-fi
