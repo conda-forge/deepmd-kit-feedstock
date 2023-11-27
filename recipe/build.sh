f95ec877e3bc921cd69705de286a754235a915db
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-11-27 16:00:24 -0500

    copy activate scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -30,3 +30,11 @@ cmake -D USE_TF_PYTHON_LIBS=TRUE \
 	  $SRC_DIR/source
 make #-j${CPU_COUNT}
 make install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
