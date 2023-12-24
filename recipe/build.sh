152deaea26f2bddfa3705c0d458a0f6c1f609ec8
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-12-24 07:32:29 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
     export TENSORFLOW_ROOT=${SP_DIR}/tensorflow
     export CMAKE_ARGS="${CMAKE_ARGS} -D TENSORFLOW_ROOT=${TENSORFLOW_ROOT}"
 fi
-export LDFLAGS="-labsl_status -labsl_raw_logging_internal ${LDFLAGS}"
+export LDFLAGS="-labsl_status -labsl_logging_internal ${LDFLAGS}"
 DP_VARIANT=${DP_VARIANT} \
 	SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION python -m pip install . -vv
 
