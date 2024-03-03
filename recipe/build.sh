51fc72e7a198c79a63becc62ea55fc6db5980ba4
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-03-03 14:35:43 -0500

    set TORCH_CUDA_ARCH_LIST for libtorch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,17 @@
 set -e
 
+# for libtorch
+if [[ ${cuda_compiler_version} == 11.2 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+elif [[ ${cuda_compiler_version} != "None" ]]; then
+    echo "unsupported cuda version."
+    exit 1
+fi
+
 if [[ ${cuda_compiler_version} != "None" ]]; then
     DEEPMD_USE_CUDA_TOOLKIT=TRUE
     DP_VARIANT=cuda
