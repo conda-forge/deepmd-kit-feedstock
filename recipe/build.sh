31426bbd0081bad32ff613ee57dab0568301da27
Jinzhe Zeng <njzjz@qq.com>
2025-11-04 19:48:46 +0800

    fix protobuf issue; downgrade pytorch for macos

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,7 +31,7 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${mpi}" == "openmpi" ]]; then
   export OPAL_PREFIX="$PREFIX"
 fi
 # TF and PT find protobuf conflict
-export CMAKE_ARGS="${CMAKE_ARGS} -D CMAKE_FIND_PACKAGE_PREFER_CONFIG=TRUE"
+sed -i "4d" ${SP_DIR}/torch/share/cmake/Caffe2/public/protobuf.cmake
 # -labsl_log_flags is the workaround for https://github.com/conda-forge/abseil-cpp-feedstock/issues/79
 export LDFLAGS="-labsl_log_flags -labsl_status -labsl_log_internal_message -labsl_hash ${LDFLAGS}"
 DP_VARIANT=${DP_VARIANT} \
