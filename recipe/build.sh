6ecc5f118ceb483cf7ce31171eb9ffbde1cc9d55
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2025-11-05 01:17:31 +0800

    fix: add patch to improve CUDA C++ standard setting and adjust protobuf handling for macOS
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,7 +31,11 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${mpi}" == "openmpi" ]]; then
   export OPAL_PREFIX="$PREFIX"
 fi
 # TF and PT find protobuf conflict
-sed -i "4d" ${SP_DIR}/torch/share/cmake/Caffe2/public/protobuf.cmake
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "4d" ${SP_DIR}/torch/share/cmake/Caffe2/public/protobuf.cmake
+else
+    sed -i "4d" ${SP_DIR}/torch/share/cmake/Caffe2/public/protobuf.cmake
+fi
 # -labsl_log_flags is the workaround for https://github.com/conda-forge/abseil-cpp-feedstock/issues/79
 export LDFLAGS="-labsl_log_flags -labsl_status -labsl_log_internal_message -labsl_hash ${LDFLAGS}"
 DP_VARIANT=${DP_VARIANT} \
