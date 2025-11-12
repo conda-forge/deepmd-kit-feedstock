7ac313f22a8f16564602f1542f5fa59fc2cb2d80
Jinzhe Zeng <njzjz@qq.com>
2025-11-12 17:45:58 +0800

    split python and C++ packages

--- recipe/bld.bat
+++
@@ -1,2 +0,0 @@
-set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
-pip install . --no-deps -vv
---
+++ recipe/build_cc.sh
@@ -0,0 +1,27 @@
+set -evx
+
+source $RECIPE_DIR/build_common.sh
+
+mkdir $SRC_DIR/source/build
+cd $SRC_DIR/source/build
+
+
+cmake -D USE_TF_PYTHON_LIBS=FALSE \
+      -D USE_PT_PYTHON_LIBS=FALSE \
+      -D ENABLE_TENSORFLOW=TRUE \
+      -D ENABLE_PYTORCH=TRUE \
+	  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+      -D USE_CUDA_TOOLKIT=${DEEPMD_USE_CUDA_TOOLKIT} \
+	  -D LAMMPS_SOURCE_ROOT=$SRC_DIR/lammps \
+	  ${CMAKE_ARGS} \
+	  $SRC_DIR/source
+make VERBOSE=1 #-j${CPU_COUNT}
+make install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
